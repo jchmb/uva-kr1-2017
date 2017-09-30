80cf29a5cc22882dfbe2e770a5dd6e8b51ebe9f7
Jochem <jochem@localhost.localdomain>
2017-09-30 19:42:48 +0200

    Added progress percentage in console

--- src/run_experiment.py
+++ src/run_experiment.py
@@ -31,15 +31,21 @@ if not os.path.exists(dimecs_directory):
     os.makedirs(dimecs_directory)
     os.makedirs('%s/%s' % (dimecs_directory, 'out'))
 
+print("Generating sudokus...")
 num_experiments = parsed_args.k
+total_count = (parsed_args.nmax - parsed_args.nmin + 1) * len(parsed_args.mperc) * 2 * parsed_args.k
+count = 0
 for N, M in parameter_iterator(parsed_args.nmin, parsed_args.nmax, parsed_args.mperc):
     gen = Sudoku3DAlternativeGenerator(N, M)
     for i in range(num_experiments):
         sudoku = gen.generate()
         converter = SudokuDimecsConverter(sudoku)
         for smart in [True, False]:
+            progress = round((float(count) / float(total_count)) * 100.0, 2)
             dimecs = converter.convert()
             dimecs.save('%s/sudoku_%s_%s_%s_%s' % (dimecs_directory, N, M, i, 1 if smart else 0))
+            count += 1
+            print('Progress: %.2f%%' % (progress,), end='\r')
 
 # Solve and write the results to a CSV file.
 field_names = ['N', 'M', 'k', 'smart', 'sat',
@@ -50,10 +56,13 @@ solver = DimecsSolver(dimecs_directory)
 results = solver.solve_all_in_dir()
 mperc_str = "_".join(map(str, parsed_args.mperc))
 timestamp = datetime.datetime.now().strftime("%y%m%d%H%M%s")
+print("Solving sudokus...")
 with open('%s/results_%s_%s_%s_%s_%s.csv' % (directory, parsed_args.nmin, parsed_args.nmax, mperc_str, parsed_args.k, timestamp), 'w') as f:
+    count = 0
     writer = csv.DictWriter(f, field_names)
     writer.writeheader()
     for res in results:
+        progress = round(float(count) / float(total_count), 2)
         match = re.search('sudoku_([0-9]+)_([0-9]+)_([0-9]+)_([0-9])', res.name)
         line = dict(res.parsed_data)
         line['N'] = int(match.group(1))
@@ -61,3 +70,6 @@ with open('%s/results_%s_%s_%s_%s_%s.csv' % (directory, parsed_args.nmin, parsed
         line['k'] = int(match.group(3))
         line['smart'] = bool(int(match.group(4)))
         writer.writerow(line)
+        count += 1
+        print('Progress: %.2f%%' % (progress,), end='\r')
+print("Finished solving sudokus!")
