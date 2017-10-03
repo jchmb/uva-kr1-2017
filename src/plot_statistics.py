648f2b444e1778ad7ee907141fd81e357dbec8fb
Jochem <jochem@wcw-stud-145-109-33-166.wireless.uva.nl>
2017-10-03 10:08:24 +0200

    Fixed plot_statistics.py by sorting the values, added cleaner cli  and added bash file for plotting

--- src/plot_statistics.py
+++ src/plot_statistics.py
@@ -4,14 +4,22 @@ import statistics
 from collections import defaultdict
 import matplotlib
 import matplotlib.pyplot as plt
+from argparse import ArgumentParser
 
 percentages = list(range(10, 100, 10))
 
 output_variables = ['restarts', 'conflicts', 'decisions', 'conflict_literals',
     'propagations', 'cpu_time', 'clauses', 'variables']
 
-filename = sys.argv[1]
-plot_name = sys.argv[2]
+parser = ArgumentParser(description='Draw a plot of a given statistic')
+parser.add_argument('filename', type=str, help='Filename of the data source')
+parser.add_argument('variable', type=str, help='Variable to put on the y axis')
+parser.add_argument('--ylim-min', type=float, help='Lower bound for the y axis')
+parser.add_argument('--ylim-max', type=float, help='Upper bound for the y axis')
+args = parser.parse_args()
+
+filename = args.filename
+plot_name = args.variable
 grouped_results = defaultdict(list)
 with open(filename, 'r') as f:
     reader = csv.DictReader(f)
@@ -42,14 +50,25 @@ for param_setting, calc in aggregate[plot_name].items():
     data_y[smart][M].append(calc['mean'])
     data_x[smart][M].append(N)
 
+for smart in data_x.keys():
+    for M in data_x[smart].keys():
+        data = list(zip(data_x[smart][M], data_y[smart][M]))
+        data = sorted(data, key=lambda pair: pair[0])
+        data_x[smart][M] = [pair[0] for pair in data]
+        data_y[smart][M] = [pair[1] for pair in data]
+
 plt.figure(figsize=(10, 8))
 plt.title("%s" % (plot_name))
 plt.xlabel('N')
 plt.ylabel(plot_name)
-# plt.ylim(0, 0.01)
+if hasattr(args, 'ylim_min'):
+    plt.gca().set_ylim(bottom=args.ylim_min)
+if hasattr(args, 'ylim_max'):
+    plt.gca().set_ylim(top=args.ylim_max)
 smart = True
 for M in data_x[smart].keys():
-    if M > 50:
-        plt.plot(data_x[smart][M], data_y[smart][M], label="%s" % (M,))
+    # if M > 50:
+    # print(data_x[smart][M])
+    plt.plot(data_x[smart][M], data_y[smart][M], label="%s" % (M,))
 plt.legend()
 plt.show()
